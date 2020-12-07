22031a1f72efed00934b5561da79c460915c28ea
Seth Kerr <seth.kerr@talgov.com>
2020-12-07 13:11:36 -0500

    ViewImports

--- ASPNETCoreWebApplicationMVCTemplate/Controllers/HomeController.cs
+++ ASPNETCoreWebApplicationMVCTemplate/Controllers/HomeController.cs
@@ -17,7 +17,22 @@ namespace ASPNETCoreWebApplicationMVCTemplate.Controllers
         [Route("[action]")]
         public ViewResult Index()
         {
-            return View();
+            List<Student> listStudents = new List<Student>()
+            {
+               new Student() { StudentId = 101, Name = "James", Branch = "CSE", Section = "A", Gender = "Male" },
+               new Student() { StudentId = 102, Name = "Smith", Branch = "ETC", Section = "B", Gender = "Male" },
+               new Student() { StudentId = 103, Name = "David", Branch = "CSE", Section = "A", Gender = "Male" },
+               new Student() { StudentId = 104, Name = "Sara", Branch = "CSE", Section = "A", Gender = "Female" },
+               new Student() { StudentId = 105, Name = "Pam", Branch = "ETC", Section = "B", Gender = "Female" }
+            };
+            return View(listStudents);
+        }
+
+        [Route("[action]/{id?}")]
+        public ViewResult Details(int Id)
+        {
+            var studentDetails = new Student() { StudentId = Id, Name = "James", Branch = "CSE", Section = "A", Gender = "Male" };
+            return View(studentDetails);
         }
 
         [Route("[action]")]
